ffe240754af6d42b8506744a47c232b2c8967ea6
Jacob Baldwin <baldwin.jacob1997@gmail.com>
2019-08-13 22:03:25 -0600

    Addition of music store recommendation, as well as reviews. Updated logo sizing based on scroll position. Updated fonts.

--- home/script.js
+++ home/script.js
@@ -47,7 +47,49 @@ var portfolio = [{
     "photo":"../mediaStorage/mitchRomney.png"
 }];
 
+var reviews = [{
+    "reviewerName":"Amy Stoddard",
+    "relationship":"Student Parent",
+    "review":`We've been looking for a trombone teacher for my son for years. 
+    We finally found one we love! My son had marching band pass offs on Friday, 
+    but didn't get the music until Thursday afternoon. His teacher, in 30 
+    minutes, got him to memorize the whole song and taught him valuable lessons 
+    on memorization. I'm super impressed.`
+},
+{
+    "reviewerName":"Hailey Allen",
+    "relationship":"Student Parent",
+    "review":`My daughter had learned so much taking piano lessons here! She's 
+    now writting her own music now too! We love it.`
+},
+{
+    "reviewerName":"Keanna Zarate",
+    "relationship":"Student Parent",
+    "review":`We have loved Miles Ahead Music! We were looking for a saxophone 
+    instructor for our son and found Emily! We've been amazed at the progress 
+    he's made. She's helped him add emotion and style to his playing, he enjoys 
+    working with her and always comes home with great things to say!! Highly 
+    recommended!`
+}];
+
 createPortfolioStack();
+populateReviews();
+
+window.onscroll = function(){
+    growShrinkLogo();
+}
+
+function populateReviews(){
+    for(let i=0; i<reviews.length; i++){
+        $('.realReviews').append(`
+            <div class="singleReview id="review${i+1}">
+                <div class="reviewerName">${reviews[i].reviewerName}</div>
+                <div class="relationship">${reviews[i].relationship}</div>
+                <div class="review">"${reviews[i].review}"</div>
+            </div>
+        `)
+    }
+}
 
 function createPortfolioStack(){
     let cardWidth = 300;
@@ -70,11 +112,11 @@ function createPortfolioStack(){
         //POSITIONING OF ITEMS ON PAGE::
 
         //if even amt of projects:
-        if(portfolio.length % 2 === 0){
+        if (portfolio.length % 2 === 0){
             console.log('even amt');
             var halfNum = portfolio.length/2;
-            if(i < halfNum){
-                if(i == halfNum - 1){
+            if (i < halfNum){
+                if (i == halfNum - 1){
                     var movement = (halfNum - i) * (cardWidth/4);
                 }
                 else {
@@ -82,8 +124,8 @@ function createPortfolioStack(){
                 }
                 $('#' + portfolio[i].id + '').css('left', `${movement}px`);
             }
-            if(i >= halfNum) {
-                if(i == halfNum){
+            if (i >= halfNum) {
+                if (i == halfNum){
                     var movement = (i - (halfNum - 1)) * (cardWidth/4);
                 }
                 else {
@@ -93,14 +135,14 @@ function createPortfolioStack(){
             }
         }
         //if odd amt of projects:
-        if(portfolio.length % 2 === 1){
+        if (portfolio.length % 2 === 1){
             console.log('odd amt');
             var halfNum = (portfolio.length - 1)/2;
-            if(i < halfNum){
+            if (i < halfNum){
                 let movement = (halfNum - i) * (cardWidth/2);
                 $('#' + portfolio[i].id + '').css('left', `${movement}px`);
             }
-            if(i > halfNum){
+            if (i > halfNum){
                 let movement = (i - halfNum) * (cardWidth/2);
                 $('#' + portfolio[i].id + '').css('right', `${movement}px`);
             }
@@ -110,8 +152,9 @@ function createPortfolioStack(){
     }
     
     // adjusting z-index values to front-face current mouseover item
-    }
-    function toTopOfStack(boxId){
+}
+
+function toTopOfStack(boxId){
     for (let i = 0; i < portfolio.length; i++){
         if (portfolio[i].id === boxId){
             $('#' + portfolio[i].id + '').css('z-index', `${portfolio.length}px`);
@@ -122,5 +165,18 @@ function createPortfolioStack(){
                 $('#' + portfolio[a].id + '').css('z-index', `${portfolio.length - (i - a)}`);
             }
         }
+    }
+}
+
+function growShrinkLogo(){
+    var logo = document.getElementById("logo");
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75){
+        logo.style.width = "100px";
+        logo.style.height = "100px";
+        logo.style.top = "0px";
+    } else {
+        logo.style.width = "250px";
+        logo.style.height = "250px";
+        logo.style.top = "10px";
     }
 }
