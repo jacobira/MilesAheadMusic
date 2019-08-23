3113fdee23ca9579796950ed3f79a665211f5e7f
Jacob Baldwin <baldwin.jacob1997@gmail.com>
2019-08-22 22:54:37 -0600

    Creation of about-us page with styling and content. Further styling updates throughout.

---
+++ about-us/script.js
@@ -0,0 +1,23 @@
+window.onscroll = function(){
+    growShrinkLogo();
+}
+
+function growShrinkLogo(){
+    var logo = document.getElementById("logo");
+    if (document.body.scrollTop > 75 || document.documentElement.scrollTop > 75){
+        logo.style.width = "100px";
+        logo.style.height = "100px";
+        logo.style.top = "0px";
+    } else {
+        logo.style.width = "250px";
+        logo.style.height = "250px";
+        logo.style.top = "10px";
+    }
+}
+
+function displayForm(){
+    document.getElementById("form").classList.remove("hidden");
+}
+function closeForm(){
+    document.getElementById("form").classList.add("hidden");
+}
